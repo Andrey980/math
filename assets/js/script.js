1caeb7aa38ba36b70b2786b8b8ffb9843c5c9f68
LucasCamposGit <lucascpn2501@gmail.com>
2023-01-03 08:02:11 -0300

    detalhes

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,29 @@ const cancel = document.querySelector("#cancel");
 
 search.addEventListener('focus', () =>{
   form_search.style.width = "12vw";
+  form_search.style.padding = "0 6.7vw";
+  form_search.style.gap = "0.2vw"
   search.style.width = "8vw";
   search.style.animation = "blink 1s infinite";
-  cancel.style.display = "block";
+  cancel.style.display = "flex";
+})
+
+search.addEventListener('input', () => {
+  search.style.animation = "none";
+  search.style.textDecoration = "none";
+})
+
+search.addEventListener('blur', () => {
+  form_search.style.width = "5vw";
+  form_search.style.padding = "0"
+  search.style.textDecoration = 'underline'
+  search.style.width = "1vw";
+  search.style.animation = "none";
+  search.value = '    ';
+  cancel.style.display = "none";
 })
 
 cancel.addEventListener('click', () =>{
-  search.blur();
+  search.blur(); 
+  
 })
