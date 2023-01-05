ec64ef155ca42b7dd8ea4baa3894bcd0cd76d776
Andrey980 <andreymcunhasilva@gmail.com>
2023-01-05 20:59:48 -0300

    Em andamento

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,39 @@
 const form_search = document.querySelector("#form_search");
 const search = document.querySelector("#form-search");
 const cancel = document.querySelector("#cancel");
+const theme = document.querySelector("#theme");
+
+const references = document.querySelector("#references");
+const guides = document.querySelector("#guides");
+const math_plus = document.querySelector("#math-plus")
+
+const ref_nav = document.querySelector("#nav-references")
+
+const ul_nav = document.querySelectorAll("header>nav>ul>li>ul");
+
+references.addEventListener('mouseover', (e)=>{
+  ul_nav[0].style.display = "block"
+})
+
+references.addEventListener('mouseout', ()=>{
+  ul_nav[0].style.display = "none"
+})
+
+guides.addEventListener('mouseover', () => {
+  ul_nav[1].style.display = "block"
+})
+
+guides.addEventListener('mouseleave', () => {
+  ul_nav[1].style.display = "none"
+})
+
+math_plus.addEventListener('mouseover', () =>{
+  ul_nav[2].style.display = "block"
+})
+
+math_plus.addEventListener('mouseleave', () =>{
+  ul_nav[2].style.display = "none"
+})
 
 
 search.addEventListener('focus', () =>{
@@ -31,3 +64,4 @@ cancel.addEventListener('click', () =>{
   search.blur(); 
   
 })
+
